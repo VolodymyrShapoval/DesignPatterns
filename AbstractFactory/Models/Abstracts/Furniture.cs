7798726336cc8b315892f975732cd3ec9684f365
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-03-25 16:46:23 +0200

    feat: implement abstract factory in furniture factory

---
+++ AbstractFactory/Models/Abstracts/Furniture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.Models.Abstracts
+{
+    public abstract class Furniture
+    {
+        public abstract void DisplayInfo();
+    }
+}
